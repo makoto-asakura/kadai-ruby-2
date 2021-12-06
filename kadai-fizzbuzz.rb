a85c10451b647c628fe099ff1604c67bd47d1ee5
makoto.asakura <makoto@asakula.com>
2021-12-06 13:34:33 +0000

    modify

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,19 +1,17 @@
-num_max = 100
-
 #メソッドの定義
 def fizzbuzz(num)
   if num%15==0
-    result="FizzBuzz"
+    "FizzBuzz"
   elsif num%3==0
-    result="Fizz"
+    "Fizz"
   elsif num%5==0
-    result="Buzz"
+    "Buzz"
   else 
-    result=num
+    num
   end
 end
 
 #メソッドの呼び出し
-(1..100).each do |num|
+(1..num_max = 100).each do |num|
   puts fizzbuzz(num)
 end
